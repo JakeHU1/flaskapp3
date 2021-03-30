bf9bb19ea8124758ed4545eae9b84a532537b958
JakeHU1 <43312270+JakeHU1@users.noreply.github.com>
2021-03-30 19:47:43 +0200

    Update main.py
    changed port for demo

--- main.py
+++ main.py
@@ -176,4 +176,4 @@ def delete(hostname=None, UUID=None):
 
 # Run the app on port 5000 on all interfaces, accepting only HTTPS connections
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context='adhoc', host='0.0.0.0', port=5000)
+    app.run(debug=True,  ssl_context=('certs/cert.pem','certs/cert.key'), host='0.0.0.0', port=443)
